3d3b18003b9de23053d47e4dfe96250f4f8fdd06
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-06-18 20:49:58 -0400

    add a dashed green line to represent the mean of the attendance data

--- stats/attendance.py
+++ stats/attendance.py
@@ -12,9 +12,19 @@ attendance.columns = ["year", "attendance"]
 # convert the attendance column values from string to number
 attendance.loc[:, "attendance"] = pd.to_numeric(attendance.loc[:, "attendance"])
 
-# plot and display the attendance data as a bar graph
+# plot the attendance data as a bar graph
 # figsize=(width, height) - inches
 attendance.plot(x="year", y="attendance", figsize=(15,7), kind="bar")
+
+# add labels to the axis
+plt.xlabel("Year")
+plt.ylabel("Attendance")
+
+# add a horizontal dashed green line to represent the mean of the attendance data
+# plt.axhline(y-position of line, label, linestyle, color)
+plt.axhline(y=attendance["attendance"].mean(), label="Mean", linestyle="--", color="green")
+
+# display the bar graph of attendance data
 plt.show()
 
 
