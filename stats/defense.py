ff47765433efea21c6b8c4a20f5d2e30626620e1
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-06-19 00:51:07 -0400

    display DER data for all star games within the last 40 years using a line plot

--- stats/defense.py
+++ stats/defense.py
@@ -8,7 +8,6 @@ plays = games.query("type == 'play' & event != 'NP'")
 
 # rename the column headers
 plays.columns = ["type", "inning", "team", "player", "count", "pitches", "event", "game_id", "year"]
-print(plays)
 
 # remove duplicate at-bat data to calculate plate appearances only
 # shift() - shift index by a desired number
@@ -21,11 +20,44 @@ pa = plays.loc[plays["player"].shift() != plays["player"], ["year", "game_id", "
 pa = pa.groupby(["year", "game_id", "team"]).size().reset_index(name="PA")
 
 # set the index of events to four columns (year, game_id, team, event_type)
-print(events)
 events = events.set_index(["year", "game_id", "team", "event_type"])
-print(events)
 
+# unstack the events data back into a table view and fill empty values with 0
 events = events.unstack().fillna(0).reset_index()
 
-print(pa)
-print(events)
+print(events)  # before column level drop
+# drop a layer of column names
+events.columns = events.columns.droplevel()
+
+# rename the column headers
+events.columns = ["year", "game_id", "team", "BB", "E", "H", "HBP", "HR", "ROE", "SO"]
+events = events.rename_axis(None, axis="columns")
+
+# merge the plate appearances with the events - both DataFrames have similar columns
+events_plus_pa = pd.merge(events, pa, how="outer", left_on=["year", "game_id", "team"],
+                          right_on=["year", "game_id", "team"])
+
+# merge in DataFrame "info" containing data on which team was the home team and which was the away team
+defense = pd.merge(events_plus_pa, info)
+
+# calculate the DER - Defensive Efficiency Ratio into a new column for every row
+defense.loc[:, "DER"] = 1 - ((defense["H"] + defense["ROE"]) / (
+            defense["PA"] - defense["BB"] - defense["SO"] - defense["HBP"] - defense["HR"]))
+
+# convert the values in the year column from string to number
+defense.loc[:, "year"] = pd.to_numeric(defense["year"])
+
+# retrieve the der data within the past 40 years
+der = defense.loc[defense["year"] >= 1978, ["year", "defense", "DER"]]
+
+# adjust data for plotting
+der = der.pivot(index="year", columns="defense", values="DER")
+
+# display the DER data using a line plot
+# x_compat - suppress the default tick behavior
+der.plot(x_compat=True, xticks=range(1978, 2018, 4), rot=45)
+plt.xlabel("Year")
+plt.ylabel("Defensive Efficiency Ratio")
+plt.show()
+
+print(der)
